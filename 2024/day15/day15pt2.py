011c3269e4ab94e8f19ac0631d36d73eddad1ce1
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-12-19 00:42:43 -0500

    finished day15

--- 2024/day15/day15pt2.py
+++ 2024/day15/day15pt2.py
@@ -12,28 +12,58 @@ def parse(filename="input.txt"):
     return newmap, m, posA
 def moveBoulderX(map, direction, p):
     cp = p[1]
-    while 0 <= cp and cp < len(map[0]):
+    if 0 <= cp and cp < len(map[0]):
         cp += direction
         if map[p[0]][cp] == '.':
-            map[p[0]].pop(cp)
-            map[p[0]].insert(p[0],'.')
+            map[p[0]][cp] = map[p[0]][cp-direction]
             return True
         elif map[p[0]][cp] == '#':
             return False
+        else:
+            previous = map[p[0]][cp-direction]
+            result = moveBoulderX(map,direction,[p[0],p[1]+direction])
+            if result:
+                map[p[0]][cp] = previous
+            return result
     return False
-def moveBoulderY(map, dir, p):
-    if 0 <= p and p < len(map):
+def moveBoulderY(map, dir, p, action=False):
+    if 0 <= p[0] and p[0] < len(map) and 0 <= p[1] and p[1] < len(map[0]):
         if map[p[0]][p[1]] == '.':
             return True
         elif map[p[0]][p[1]] == '[':
-            r0 = moveBoulderY(map,dir,[p[0]+dir,p[1]-dir])
-            r1 = moveBoulderY(map,dir,[p[0]+dir,p[1]-dir])
-            return r0 and r1
+            r0 = moveBoulderY(map,dir,[p[0]+dir,p[1]],action=action)
+            r1 = moveBoulderY(map,dir,[p[0]+dir,p[1]+1],action=action)
+            result = r0 and r1
+            if result:
+                if action:
+                    map[p[0]+dir][p[1]] = '['
+                    map[p[0]+dir][p[1]+1] = ']'
+                    map[p[0]][p[1]] = '.'
+                    map[p[0]][p[1]+1] = '.'
+
+            return result
         elif map[p[0]][p[1]] == ']':
-            r0 = moveBoulderY(map,dir,[p[0]+dir,p[1]-dir])
-            r1 = moveBoulderY(map,dir,[p[0]+dir,p[1]-dir])
-            return r0 and r1
-        
+            r0 = moveBoulderY(map,dir,[p[0]+dir,p[1]],action=action)
+            r1 = moveBoulderY(map,dir,[p[0]+dir,p[1]-1],action=action)
+            result = r0 and r1
+            if result:
+                if action:
+                    map[p[0]+dir][p[1]] = ']'
+                    map[p[0]+dir][p[1]-1] = '['
+                    map[p[0]][p[1]] = '.'
+                    map[p[0]][p[1]-1] = '.'
+            return result
+
+def moveBoulder(map, dir, newPos):
+    if dir[1]:
+        result = moveBoulderX(map,dir[1],newPos)
+    else:
+        result = moveBoulderY(map,dir[0],newPos)
+        if result:
+            moveBoulderY(map,dir[0],newPos,action=True)
+    if result:
+        map[newPos[0]][newPos[1]] = '.'
+    return result
 
 def applyMove(map,pos,move):
     if move == 'v':
@@ -53,7 +83,7 @@ def applyMove(map,pos,move):
             return newPos
         elif blocker == '#':
             return pos
-        elif blocker == 'O':
+        elif blocker in ['[',']']:
             if moveBoulder(map,direction,newPos):
                 return newPos
             else:
@@ -64,7 +94,7 @@ def calculateGPS(map):
     count = 0
     for i,line in enumerate(map):
         for ii,c in enumerate(line):
-            if map[i][ii] == 'O':
+            if map[i][ii] == '[':
                 count += (i * 100)
                 count += ii
     return count
@@ -77,7 +107,7 @@ def printMap(map,pos):
                 print(c,end="")
         print("")
     print("")
-map, moves, startP = parse()
+map, moves, startP = parse(filename='input.txt')
 cp = startP.copy()
 printMap(map,cp)
 for move in moves:
