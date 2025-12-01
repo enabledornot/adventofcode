6550de1d05ad9fcf12ed7750428ac0d0ad42c61a
enabledornot <102371948+enabledornot@users.noreply.github.com>
2025-12-01 00:49:17 -0500

    added better day01 code

---
+++ 2025/day01/day01pt2_better.py
@@ -0,0 +1,38 @@
+def parse():
+    with open('input.txt',"r") as f:
+        return f.read().split("\n")
+
+pos = 50
+cnt = 0
+for line in parse():
+    # print(f"{pos}-{cnt}")
+    if len(line) < 1:
+        continue
+    if int(line[1:]) == 0:
+        continue
+    if line[0] == "R":
+        npos = int(line[1:])
+    elif line[0] == "L":
+        npos = - int(line[1:])
+    else:
+        print('err')
+    # print(npos)
+    # print("")
+    cnt += abs(npos) // 100
+    if npos < 0:
+        npos = (-1*npos) % 100
+        npos = -1*npos
+    else:
+        npos = npos % 100
+    opos = pos
+    pos = pos + npos
+    if pos < 0:
+        pos += 100
+        if opos != 0:
+            cnt += 1
+    elif pos >= 100:
+        pos -= 100
+        cnt += 1
+    elif pos == 0:
+        cnt += 1
+print(cnt)
